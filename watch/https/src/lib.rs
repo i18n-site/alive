f5b5227d9386b6dae1b4605f4d82427c999d8663
i18n <i18n.site@gmail.com>
2024-06-27 16:32:27 +0800

    Https supports domain names that distinguish between IPv4 and IPv6 https支持区分ipv4和ipv6的域名

--- watch/https/src/lib.rs
+++ watch/https/src/lib.rs
@@ -5,8 +5,15 @@ use aok::{Result, OK};
 use tls_ping::tls_ping;
 
 #[derive(serde::Deserialize, Debug)]
+pub struct HostIp {
+  v4: Option<Vec<String>>,
+  v6: Option<Vec<String>>,
+  v4v6: Option<Vec<String>>,
+}
+
+#[derive(serde::Deserialize, Debug)]
 pub struct Conf {
-  cluster: HashMap<String, Vec<String>>,
+  cluster: HashMap<String, HostIp>,
   host: HashMap<String, HashMap<String, Vec<String>>>,
 }
 
@@ -22,27 +29,31 @@ const TIMEOUT: u64 = 30;
 pub async fn load(wc: &alive_watch::Conf) -> Result<Vec<Task<Arg>>> {
   let c: Conf = alive_watch::yml!(wc);
   let mut r = Vec::new();
-  for (cluster, host_li) in c.cluster {
+  for (cluster, host_ip) in c.cluster {
     for (hostname, ip) in wc.cluster(cluster)?.iter() {
       macro_rules! push {
-        ($attr:ident, $v:ident, $tag:ident) => {
+        ($attr:ident, $ipv:ident, $v:ident, $tag:ident) => {
           for i in &ip.$attr {
-            for host in &host_li {
-              r.push(Task::new(
-                host.clone(),
-                Arg {
-                  ip: IpAddr::$v(*i),
-                  host: host.into(),
-                },
-                [hostname.into(), stringify!($tag).into()],
-                DURATION,
-              ))
+            if let Some(li) = &host_ip.$ipv {
+              for host in li {
+                r.push(Task::new(
+                  host.clone(),
+                  Arg {
+                    ip: IpAddr::$v(*i),
+                    host: host.into(),
+                  },
+                  [hostname.into(), stringify!($tag).into()],
+                  DURATION,
+                ))
+              }
             }
           }
         };
       }
-      push!(ipv4_li, V4, IPV4);
-      push!(ipv6_li, V6, IPV6);
+      push!(ipv4_li, v4, V4, IPV4);
+      push!(ipv6_li, v6, V6, IPV6);
+      push!(ipv4_li, v4v6, V4, IPV4);
+      push!(ipv6_li, v4v6, V6, IPV6);
     }
   }
   for (cluster, vps_host_map) in &c.host {
